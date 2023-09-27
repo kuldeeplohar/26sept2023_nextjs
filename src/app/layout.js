96f60f8a9bbf0adccc0fae06d5aa406cd35d4b0f
kuldeeplohar <loharkuldeep169@gmail.com>
2023-09-27 12:15:24 +0530

    c1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,13 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import './globals.css'
 import { Inter } from 'next/font/google'
+
+import Header from '@/components/layout/header'
+import Footer from '@/components/layout/footer'
+import Aside from '@/components/layout/aside'
+import Aside2 from '@/components/layout/aside2'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,9 +17,22 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+     
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="container p-0 k_tbdr">
+    <Header/>
+    <main>
+      <div className="row h-100 m-0"> 
+         <Aside/>
+        {children}
+        <Aside2/>
+      </div>
+    </main>
+    <Footer/>
+    </div> 
+        </body>
     </html>
   )
 }
